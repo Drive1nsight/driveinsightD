40c6a48bbcd4fcf3f83c0d6f9ab3866bb7f7fb09
sultan91 <sultan.yerumbayev@gmail.com>
2025-11-19 00:16:53 +0500

    update script add maps

--- set_up.sh
+++ set_up.sh
@@ -2,5 +2,12 @@
 
 wget https://github.com/esmini/esmini/releases/download/v2.35.0/esmini-demo_Linux.zip
 unzip -o esmini-demo_Linux.zip
-wget -O cz_zlin.zip https://drive.usercontent.google.com/download\?id\=17hpIfkTbzxwuq5smXrGWhzDNGjIo4thL\&export\=download\&authuser\=2\&confirm\=t\&uuid\=8837b9d8-76d0-4453-bbe6-1eb977d645c6\&at\=APcmpozACqK1fbDbLH20vxzjs22i:1744536666354
-unzip cz_zlin.zip -d scenarios
+
+wget -O cz_zlin_osgb.zip https://drive.google.com/uc?id=1tyDfd20Vcj0h8brzwn6VQrxfSZ-f4I60
+unzip cz_zlin_osgb.zip -d database/cz_zlin
+
+wget -O us_coldwater_osgb.zip https://drive.google.com/uc?id=1tyDfd20Vcj0h8brzwn6VQrxfSZ-f4I60
+unzip us_coldwater_osgb -d database/us_coldwater
+
+wget -O jp_taito_osgb.zip https://drive.google.com/uc?id=11h6v-PLedL1nKVtu-ZldbxabnESHGcfW
+unzip jp_taito_osgb -d database/jp_taito
