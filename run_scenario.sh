4abfd0ec443b91fa00cd6ebaffc4bf592f97675d
sultan91 <sultan.yerumbayev@gmail.com>
2025-12-05 16:27:27 +0500

    update scripts

--- run_scenario.sh
+++ run_scenario.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-
+# jp_taito 0,-120,90,1.2,0.6
+# cz_zlin 130,20,45,2.5,0.6
+# us_coldwater0,-120,90,1.2,0.6
 # Check if an argument is given
 if [ -z "$1" ]; then
   echo "Usage: $0 <filename.xosc>"
   exit 1
 fi
+SCENARIO_PATH="$1"
 
-XOSC_FILE="$1"
-
+case "$SCENARIO_PATH" in
+    *jp_taito*)
+        params_string="0,-120,90,1.2,0.6"
+        ;;
+    *cz_zlin*)
+        params_string="130,20,45,2.5,0.6"
+        ;;
+    *us_coldwater*)
+        params_string="0,-120,90,1.2,0.6"
+        ;;
+    *)
+        # Default or fallback parameters if none of the above match
+        echo "Warning: Unknown location in SCENARIO_PATH. Using default params."
+        params_string="0,0,0,1.0,1.0"
+        ;;
+esac
 # Run esmini with the given .xosc file
-esmini-demo/bin/esmini --window 60 60 1200 800 --osc "$XOSC_FILE" --custom_fixed_camera 130,20,45,2.5,0.6
+# Execute the final command with the determined parameters
+echo "Running esmini for $SCENARIO_PATH with params: $params_string"
+
+app/esmini_visualizer_app/esmini-demo/bin/esmini \
+    --window 60 60 1200 800 \
+    --osc "$SCENARIO_PATH" \
+    --custom_fixed_camera "$params_string"
--- set_up.sh
+++ set_up.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+mkdir -p app/esmini_visualizer_app
 wget https://github.com/esmini/esmini/releases/download/v2.35.0/esmini-demo_Linux.zip
-unzip -o esmini-demo_Linux.zip
+unzip -o esmini-demo_Linux.zip -d app/esmini_visualizer_app
 
-wget -O cz_zlin_osgb.zip https://drive.google.com/uc?id=1tyDfd20Vcj0h8brzwn6VQrxfSZ-f4I60
+wget -O cz_zlin_osgb.zip "https://drive.usercontent.google.com/download?id=1tyDfd20Vcj0h8brzwn6VQrxfSZ-f4I60&export=download&authuser=1&confirm=t&uuid=0780a4e4-5690-4980-a4b2-3bd2abc67afd&at=ALWLOp5rIL2CqQfS_zCl8yQAVFUO:1764925200101"
+wget -O us_coldwater_osgb.zip "https://drive.usercontent.google.com/download?id=153cCDZyPK_QAt2Wi_7GQiH9mNkNvkt0T&export=download&authuser=1&confirm=t&uuid=d6b9e6c6-e5b2-470a-84e6-658ff616b4cb&at=ALWLOp5PVq-B2RLePZPDk4d_yZQr:1764925158758"
+wget -O jp_taito_osgb.zip "https://drive.usercontent.google.com/download?id=11h6v-PLedL1nKVtu-ZldbxabnESHGcfW&export=download&authuser=1&confirm=t&uuid=62946ae1-bbb6-46e9-93df-cd7af87c47f8&at=ALWLOp68e7Oee9Q42QkL7xO9M7Z1:1764925035282"
+
+unzip jp_taito_osgb.zip -d database/jp_taito
 unzip cz_zlin_osgb.zip -d database/cz_zlin
-
-wget -O us_coldwater_osgb.zip https://drive.google.com/uc?id=1tyDfd20Vcj0h8brzwn6VQrxfSZ-f4I60
-unzip us_coldwater_osgb -d database/us_coldwater
-
-wget -O jp_taito_osgb.zip https://drive.google.com/uc?id=11h6v-PLedL1nKVtu-ZldbxabnESHGcfW
-unzip jp_taito_osgb -d database/jp_taito
+unzip us_coldwater_osgb.zip -d database/us_coldwater
+mv database/jp_taito/osgb_file.osgb database/jp_taito/jp_taito.osgb
+mv database/cz_zlin/osgb_file.osgb database/cz_zlin/cz_zlin.osgb
+mv database/us_coldwater/osgb_file.osgb database/us_coldwater/usa_coldwater.osgb
